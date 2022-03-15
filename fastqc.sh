2493be36246019665fb81ba4712158e74e925e29
Victor Hugo Calegari de Toledo <victor.toledo.btos@gmail.com>
2022-03-15 15:01:05 -0300

    Create fastqc.sh
    quality control code for RNAseq

---
+++ fastqc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#### Quality control RNAseq (FastQC) ####
+
+## Access folder with raw data
+
+cd ~/raw_data/
+
+## Creating the results folder:
+
+mkdir ../fastqc/_m
+
+# Running:
+
+for i in $(ls); do
+if [[ $i = *.gz ]]
+then
+fastqc $i -o ~/fastqc/fastqc_results &
+fi #fi closes the if
+done
+
+wait
